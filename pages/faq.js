f30f3c99e255d2d5eecc5954781aeee06a4d377a
Amon Silva <atsilva@sgr.net.br>
2022-05-20 11:08:40 -0300

    Lesson 1

---
+++ pages/faq.js
@@ -0,0 +1,8 @@
+export default function FAQPage(){
+    return (
+        <div>
+            <h1>Alura Cases - Páginas de pergunta FAQ</h1>
+            <a href="/">Ir para Home</a>
+        </div>
+    )
+}
