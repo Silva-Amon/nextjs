8ebef8ca48276178a7e8109cf97d50806246d746
Amon Silva <atsilva@sgr.net.br>
2022-05-23 16:24:32 -0300

    lesson 4

--- pages/faq.js
+++ pages/faq.js
@@ -1,12 +1,45 @@
 import Link from "../src/components/Link"
+import Head from 'next/head'
 
-export default function FAQPage(){
+export async function getStaticProps(){
+    const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json'
+    const faq = fetch(FAQ_API_URL)
+        .then((respostaDoServidor) => {
+            return respostaDoServidor.json();
+        })
+        .then((resposta) => {
+            return resposta
+        })
+    return {
+        props: {
+            qualquercoisa: 'que eu passar aqui',
+            faq
+        }
+    }
+}
+
+export default function FAQPage({faq}){
     return (
         <div>
+        <Head>
+            <title>FAQ - Alura Cases Campanha</title>
+        </Head>
             <h1>Alura Cases - Páginas de pergunta FAQ</h1>
             <Link href="/">
                 Ir para Home
             </Link>
+            <ul>
+                {faq.map(({answer, question}) => {
+                    <li key={question}>
+                        <article>
+                            <h2>{question}</h2>
+                            <article>
+                                <p>{answer}</p>
+                            </article>
+                        </article>
+                    </li>
+                })}
+            </ul>
         </div>
     )
 }
